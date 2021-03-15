6e22060d8c739a21c0035408e6f22b4adb58f987
Abrahán Fernández <afernandez.nieto@atsistemas.com>
2021-03-15 11:46:12 +0100

    First commit

---
+++ Example/TestingPrivatePod/DebugPrint.swift
@@ -0,0 +1,9 @@
+//
+//  DebugPrint.swift
+//  TestingPrivatePod_Tests
+//
+//  Created by Abrahán Fernández on 15/3/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
